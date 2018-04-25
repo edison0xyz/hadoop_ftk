e8c18f0b10a28192e70bcab5e696d977bf82e9e8
Edison <edison.limjh@gmail.com>
2018-04-25 02:30:05 -0400

    display xml data

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# hadoop_ftk
-Web interface for hadop file system extraction
+# Hadoop File System Forensics Toolkit (HDFS FTK)
+View and extract files from an offline image of Hadoop file system.
+
+
+### Motivation
+In Hadoop File Systems, metadata such as filename, access time and file system hierachy information is stored in the namenode. The actual data is stored in the datanodes in _blocks_. Although HDFS has command client tools to manage the extraction files, it only works with a running cluster of HDFS machines.
+This python script aims to provide investigators with the ability to perform forensics analysis of offline evidence captures of Hadoop File System images.
+
+
+
+## Instructions
+
+
+### PreRequisites
+* Python 3.x
+* PrettyTable: `pip install prettytable`
+
+### Usage
+
+```
+Hadoop Forensics File System Forensics Tool Kit (HDFS FTK) ver. 1.0
+usage: hdfs_ftk.py [-h] [-v] [-d D] [-o O] -f F
+
+HDFS Forensics Toolkit.
+
+optional arguments:
+  -h, --help       show this help message and exit
+  -v, --verbose    Verbose mode
+  -d D             Number of Datanodes
+  -o O, -output O  output directory
+
+Required named arguments:
+  -f F             Path to fsimage
+
+```
--- hdfs_ftk.py
+++ hdfs_ftk.py
@@ -1,31 +1,65 @@
 import sys
 import argparse
+import xml.etree.ElementTree
+from prettytable import PrettyTable
+
 
 # Global variable
 VERSION = 1.0
 OUTPUT_FILE_SPECIFIED = False
 output_directory = ''
 verbosity = False
+fsimage_path = ''
 
 
 if sys.version_info < (3, 0, 0):
     sys.stderr.write("HDFS FTK requires python version 3.0 and above, please upgrade your python installation.")
     sys.exit(1)
 
+
 def vwrite(x):
-    print(verbosity)
-    if verbosity:
+
+    if verbosity:   # write to console only if verbosity is switched on
         sys.stdout.write(x)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
 
 
-def parse_arguments(self, args):
-    print(args.verbose is not None)
+def print_fsimage_info():
+    e = xml.etree.ElementTree.parse(fsimage_path).getroot()
+    entries = []
 
-    if args.verbose is not None:
+    inode_section = e[1]  # get inode_section which contains information about the inodes
+    for inode in inode_section.findall('inode'):
+
+        file_type = inode[1].text  # Type
+        file_name = inode[2].text  # Name
+        if file_name is None:
+            file_name = '/'
+        entries.append([file_type, file_name])
+
+    t = PrettyTable(['Type', 'Name'])
+    for entry in entries:
+        t.add_row(entry)
+    print(t)
+
+
+def parse_arguments(args):
+
+    if args.verbose is True:
+        print("Verbose mode on.")
+        global verbosity
         verbosity = True
 
+    # extract path to fsimage
+    global fsimage_path
+    fsimage_path = 'test/fsimage2.xml'
+    #fsimage_path = args.f
+    vwrite("FSImage path: " + fsimage_path)
+
     if args.o is not None:
-        output_directory = args.o
+        output_directory = 'test/output/'
+        #output_directory = args.o
         vwrite("Output directory: " + output_directory)
         OUTPUT_FILE_SPECIFIED = True
 
@@ -33,17 +67,12 @@ def parse_arguments(self, args):
         for i in range(args.d):
             sys.stdout.write("Path to Device {0}\n".format(i))
 
-    sys.stderr.write(args.f)
-
 
 def main():
     # Get the version information on every output from the beginning
     # Exceptionally useful for debugging/telling people what's going on
     sys.stderr.write("Hadoop Forensics File System Forensics Tool Kit (HDFS FTK) ver. {0}\n".format(VERSION))
     sys.stderr.flush()
-
-
-
 
     # parser set-up
     parser = argparse.ArgumentParser(description='HDFS Forensics Toolkit.')
@@ -54,17 +83,13 @@ def main():
                         help="Number of Datanodes")
     parser.add_argument("-o", "-output",
                         help="output directory")
-
     requiredNamed = parser.add_argument_group('Required named arguments')
     requiredNamed.add_argument("-f", required=True,
                         help="Path to fsimage") # fsimage is a required filed
-
-
     args = parser.parse_args()
     parse_arguments(args)
 
-
-
+    print_fsimage_info()
 
 
 if __name__ == "__main__":
